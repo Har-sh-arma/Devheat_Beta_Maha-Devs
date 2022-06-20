a195c440dbaed8ded1a4f6eed1654adca2781bbe
Har-sh-arma <harinathsharma303@gmail.com>
2022-06-20 21:18:55 +0530

    Java interface code with js

--- App_folder/app/src/main/java/com/devheat/billbot/MainActivity.java
+++ App_folder/app/src/main/java/com/devheat/billbot/MainActivity.java
@@ -2,10 +2,13 @@ package com.devheat.billbot;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.webkit.JavascriptInterface;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,8 +21,25 @@ public class MainActivity extends AppCompatActivity {
         webView = findViewById(R.id.myWebView);
         WebSettings webSettings = webView.getSettings();
         webSettings.setJavaScriptEnabled(true);
+        webView.addJavascriptInterface(new WebAppInterface(this), "Android");
         webView.setWebViewClient(new Callback());
         webView.loadUrl("file:///android_asset/test.html");
+    }
+
+    public class WebAppInterface {
+        Context mContext;
+
+        /** Instantiate the interface and set the context */
+        WebAppInterface(Context c) {
+            mContext = c;
+        }
+
+        /** Show a toast from the web page */
+        @JavascriptInterface
+        public int showToast(String toast) {
+            Toast.makeText(mContext, toast, Toast.LENGTH_SHORT).show();
+            return 2;
+        }
     }
 
     private class Callback extends WebViewClient{
