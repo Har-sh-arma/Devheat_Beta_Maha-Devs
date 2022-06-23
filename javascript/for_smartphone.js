0ef42ef36ca95b24405c07f8c0933bee8c9b7d55
yogesh121546 <yogeshkumawats291@gmail.com>
2022-06-23 15:57:34 +0530

    clear table function added and some comments too

--- javascript/for_smartphone.js
+++ javascript/for_smartphone.js
@@ -1,18 +1,3 @@
-<<<<<<< HEAD
-let food = [
-  { amount: 0, name: "Samosa", price: 15, qty: 0 },
-  { amount: 0, name: "Pani Puri", price: 20, qty: 0 },
-  { amount: 0, name: "Tikki Chaat", price: 25, qty: 0 },
-  { amount: 0, name: "Indori Bhel", price: 20, qty: 0 },
-  { amount: 0, name: "Vada Pav", price: 15, qty: 0 },
-  { amount: 0, name: "Neembu Pani", price: 20, qty: 0 },
-];
-let serial_no = []; //to know which serial row has which items(i.e 0,1.2....5)
-let index = 0;
-let serial = []; //to know which item has which serial row(i.e 1,2,....6)
-let total = 0;
-
-=======
 //creating food array object for different  items
 let food = [
   {  name: "Samosa", price: 15, qty: 0,amount:0 },      //food[0] refers to samosa object
@@ -31,7 +16,6 @@ let item_to_row = []; //to save info about which item has which row
 let count = 0;        //to keep track of the total row nos
 let total_amount = 0; //saves the total amount 
 
->>>>>>> 6a1d568f24f7eb9e72057cf55c7b044927bab13f
 function display_row(row_no, item) {
   document.getElementById(row_no * 10 + 1).innerHTML = row_no;
   document.getElementById(row_no * 10 + 2).innerHTML = food[item].name;
@@ -46,20 +30,6 @@ function display_empty_row(row_no) {
   document.getElementById(row_no * 10 + 4).innerHTML = "";
 }
 
-<<<<<<< HEAD
-function display_delete_button(row_no) {
-  const btn = document.createElement("button");
-  btn.innerHTML = "-";
-  btn.id = row_no * 10;
-  document.getElementById(row_no * 10 + 3).appendChild(btn);
-  document.getElementById(row_no * 10).onclick = function hi() {
-    delete_item(serial_no[row_no]);
-  };
-}
-
-function display_total() {
-  total =
-=======
 function display_remove_button(row_no) 
 {
   const btn = document.createElement("button");
@@ -73,65 +43,12 @@ function display_remove_button(row_no)
 
 function display_total_amount() {
   total_amount =
->>>>>>> 6a1d568f24f7eb9e72057cf55c7b044927bab13f
     food[0].amount +
     food[1].amount +
     food[2].amount +
     food[3].amount +
     food[4].amount +
     food[5].amount;
-<<<<<<< HEAD
-  document.getElementById("total").innerHTML = total;
-}
-
-function add_item(item) {
-  if (food[item].qty == 0) {
-    food[item].qty++;
-    food[item].amount = food[item].qty * food[item].price;
-    index++;
-    serial_no[index] = item; //intialized serial_no arry with appropriate index(serial no) as "item"
-    serial[item] = index; //intialized that particular item has serial row no =index
-    display_row(serial[item], item);
-    display_total();
-    display_delete_button(serial[item]);
-  } else {
-    food[item].qty++;
-    food[item].amount = food[item].qty * food[item].price;
-    display_row(serial[item], item);
-    display_total();
-    display_delete_button(serial[item]);
-  }
-}
-function delete_item(item) {
-  if (food[item].qty > 1) {
-    food[item].qty--;
-    food[item].amount = food[item].qty * food[item].price;
-    display_row(serial[item], item);
-    display_delete_button(serial[item]);
-    display_total();
-  } else {
-    food[item].amount = 0;
-    if (serial[item] == index) {
-      index--;
-      food[item].qty--;
-      display_empty_row(serial[item]);
-      serial[item] = item;
-      display_total();
-    } else if (serial[item] < index) {
-      food[item].qty--;
-      for (let i = serial[item]; i < index; i++) {
-        serial_no[i] = serial_no[i + 1];
-        serial[serial_no[i]]--;
-        display_row(i, serial_no[i]);
-        display_delete_button(i);
-      }
-      display_total();
-      display_empty_row(index);
-      index--;
-    }
-  }
-}
-=======
   document.getElementById("total").innerHTML = total_amount;
 }
 
@@ -200,4 +117,3 @@ function clear_table()
   }
   document.getElementById("total").innerHTML="";
 }
->>>>>>> 6a1d568f24f7eb9e72057cf55c7b044927bab13f
